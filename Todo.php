9ca243b60b4453ba53bd600b4cdbd9a4710b5a5c
Clayton Cooper <Claycooper8@gmail.com>
2021-08-13 13:25:18 -0400

    adding functionality

--- Todo.php
+++ Todo.php
@@ -8,61 +8,62 @@
         <div class="p-4">
             <div class="flex justify-center w-full text-center">
                 <input class="flex p-2 text-gray-500 w-2/3" type="text" id="A3-yes" name="mytextarea" value="Title..." />
-                <button onclick="addNewTodo()" class="flex bg-red-500 px-10">
+                <button class="flex bg-red-500 px-10">
                     <h1 class="m-auto font-bold">Add</h1>
                 </button>
             </div>
         </div>
-        <ul class="py-4">
-            <li class="delete flex justify-center w-full py-2">
-                <span class="flex bg-white p-2 text-gray-500 w-2/3 bg-white text-left">Hello</span>
-                <div class="flex bg-white hover:bg-red-500 text-3xl text-black hover:text-white w-10 
-                cursor-pointer">
-                    <div class="mx-auto">×</div>
-                </div>
-            </li>
-            <li class="delete flex justify-center w-full py-2">
-
+        <ul>
+            <li class="flex justify-center w-full py-4">
+                <div class="checked bg-white p-2 text-gray-500 w-2/3 bg-white text-left">Hello</div>
             </li>
         </ul>
     </div>
 </div>
 
 
-<!-- <label class="" for="myCheckbox" class="select-none"></label> -->
-
-
 <?php include __DIR__ . '/includes/footer.php'; ?>
 <script>
+    // \u2713 for checkmark \u00D7
 
     var LiTag = document.getElementsByTagName("LI");
     var i;
     for (i = 0; i < LiTag.length; i++) {
-        var makeSpanTag = document.createElement("SPAN");
-        var makeText = document.createElement("\u00D7");
-        makeSpanTag.className = "delete";
-        makeSpanTag.appendChild(makeText);
-        LiTag[i].appendChild(makeSpanTag);
+        //creates Span tag with X in it
+        var xSpan = document.createElement("SPAN");
+        var makeX = document.createTextNode("\u00D7");
+        //creates Span tag with checkmark in it
+        var checkSpan = document.createElement("SPAN");
+        var makeCheckmark = document.createTextNode("\u2713");
+        //since we are using tailwind, it is a good idea to add the whole class in here so it all loads
+        //These are both making the clases with the names of them with the tailwind CSS included
+        checkSpan.className = "op flex justfiy-end bg-white hover:bg-red-500 text-3xl text-black hover:text-white px-2 cursor-pointer";
+        xSpan.className = "close flex justfiy-end bg-white hover:bg-red-500 text-3xl text-black hover:text-white px-2 cursor-pointer";
+        //this basically inserts the checkmark before the div so that it can be at the front of the whole statement
+        checkSpan.appendChild(makeCheckmark);
+        LiTag[i].insertBefore(checkSpan, LiTag[i].firstChild);
+        //Inserts the X at the end of the statement
+        xSpan.appendChild(makeX);
+        LiTag[i].appendChild(xSpan)
+
     }
-    //for some reason when I had delete as an Id it didn't work but making it into a class worked
-    // var close = document.getElementsByID("delete");
-    //classes are better because you can have multiple selectors
-    var close = document.getElementsByClassName("delete");
+    //This section takes the "close" class that we made up above and added to the span tag in the X and closes the whole statement
+    //just sets all the information to no styling so it doesn't show up
+    var close = document.getElementsByClassName("close");
     var i;
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
-            var li = this.parentElement;
-            li.style.display = "none";
+            var div = this.parentElement;
+            div.style.display = "none";
         }
     }
 
-    // var closeLi = document.getElementsByTagName("LI"); //This gets the HTML tag
-    // var i;
-    //     for (i = 0; i < closeLi.length; i++) {
-    //         var div = document.createElement("DIV");
-    //         var txt = document.createTextNode("\u00D7");
-    //         div.className = "close";
-    //         div.appendChild(txt);
-    //         closeLi[i].appendChild(div);
-    //     }
+    var list = document.querySelector("LI");
+    list.addEventListener("click", function(ev) {
+        var element = ev.target;
+        if (element.tagName === 'LI') {
+            element.classList.toggle('checked');
+            console.log("testing");
+        }
+    }, false);
 </script>
