f17d2630617af73c257393e02d70cd16f1a8b712
Clayton Cooper <Claycooper8@gmail.com>
2021-08-23 09:14:46 -0400

    added comments to my code to read easier

--- Todo.php
+++ Todo.php
@@ -55,30 +55,37 @@
 
 
 
-
+        //these two with .className creates a class with this as the name in the li and in the div
         li.className = "flex justify-center w-full py-4";
         div.className = "info bg-white p-2 text-gray-500 w-2/3 bg-white text-left";
+        //appendChild puts the parameter inside of the item
         div.appendChild(input_text);
 
+        //basically if there is nothing typed it will alert you to put something in there before adding the child
         if (my_input === '') {
             alert("You Need To Add Something To The Input Text");
         } else {
             get_id("myUL").appendChild(li).appendChild(div);
         }
+        //after you send the information to away this clears out the value inside the text area
         get_id("myInput").value = "";
-
+        //declaring a new class and assigning it a text field for the checkmark
         new_checkmark_span.className = "unchecked flex justfiy-end bg-white text-3xl text-black px-2 cursor-pointer";
         new_checkmark_span.appendChild(txt_checkmark);
+        //insertbefore basically tells the child where to be put in the list of children
         li.insertBefore(new_checkmark_span, li.firstChild);
+        //new class and text
         new_x_span.className = "close flex justfiy-end bg-white text-3xl text-black px-2 cursor-pointer";
         new_x_span.appendChild(txt_x);
         li.appendChild(new_x_span);
+        //This loops through each close class and says that everytime is is clicked it runs a function that clears the styling
         for (i = 0; i < close.length; i++) {
             close[i].onclick = function() {
                 var div = this.parentElement;
                 div.style.display = "none";
             }
         }
+        //gets the unchecked class then runs it through a loop that decides if it is clicked or not and applies classes depending on the status
         var clicked = get_class("unchecked");
         for (i = 0; i < clicked.length; i++) {
             clicked[i].onclick = function(e) {
